ce953cffb3c0ed909e055bbae55e672d97e7d999
shangdibaozi <shangdibaozi@qq.com>
2021-09-06 22:42:07 +0800

    拖尾

--- assets/Scripts/Core/Player.ts
+++ assets/Scripts/Core/Player.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, Vec3, toRadian, toDegree, v3, CCFloat } from 'cc';
+import { _decorator, Component, Node, Vec3, toRadian, toDegree, v3, CCFloat, RigidBody2D, Vec2, CCLoader } from 'cc';
 import { UI_EVENT } from '../Constants';
 import { Global } from '../Global';
 
@@ -9,13 +9,22 @@ let playerPos = v3();
 const { ccclass, property } = _decorator;
 @ccclass('Player')
 export class Player extends Component {
+    speed: number = 0;
+
     @property(CCFloat)
-    speed: number = 1;
+    maxSpeed: number = 100;
+
+    @property(CCFloat)
+    acceleration: number = 100;
 
     curHeading: Vec3 = v3(1, 0, 0);
     targetHeading: Vec3 = v3(1, 0, 0);
 
     onLoad() {
+        this.installEvents();
+    }
+
+    installEvents() {
         Global.uiEvent.on(UI_EVENT.PLAYER_MOVE, this.onPlayerMove, this);
     }
 
@@ -32,9 +41,17 @@ export class Player extends Component {
             this.node.angle = toDegree(Math.atan2(this.curHeading.y, this.curHeading.x));
             console.log(this.node.angle);
         }
-        
+
+        if(this.speed < this.maxSpeed) {
+            this.speed = Math.min(this.speed + this.acceleration * dt, this.maxSpeed)
+        }
+
         playerPos.set(this.node.position);
         playerPos.add3f(this.curHeading.x * this.speed * dt, this.curHeading.y * this.speed * dt, 0);
         this.node.setPosition(playerPos);
+    }
+
+    lateUpdate() {
+
     }
 }
